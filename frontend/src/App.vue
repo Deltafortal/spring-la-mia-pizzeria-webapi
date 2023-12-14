62f29e3ede59dfc0632b0d22519ac28b36f404f1
Deltafortal <aldolucchetta21@gmail.com>
2023-12-14 16:00:02 +0100

    Added index

---
+++ frontend/src/App.vue
@@ -0,0 +1,63 @@
+<!------------------ Script ----------------->
+<script>
+    //Import
+    import AppHeader from './components/AppHeader.vue';
+    import AppMain from './components/AppMain.vue';
+    import axios from 'axios';
+
+    export default {
+        components: { AppHeader, AppMain },
+
+        //Data
+        data() {
+            return {
+              pizzas: [],
+            }
+        },
+
+        //methods
+        methods: {
+
+          fetchPizza(){
+
+            axios.get('http://localhost:8080/api/pizza').then(response => {
+              this.pizzas = response.data;
+            }).catch(error => {
+              console.error('Errore nella chiamata API', error);
+            });
+          }
+
+        },
+
+
+        // Mounted
+        mounted() {
+          this.fetchPizza()
+        }
+
+    } 
+</script>
+
+
+
+
+<!------------------ Template ----------------->
+<template>
+
+  <!--- Header-->
+  <AppHeader />
+
+  <div class="container mb-5">
+    <!--- Main -->
+    <AppMain :pizzas="this.pizzas" />
+  </div>
+
+</template>
+
+
+
+
+<!------------------ Style ----------------->
+<style lang="scss">
+
+</style>
---
+++ frontend/src/components/AppMain.vue
@@ -0,0 +1,65 @@
+<!------------------ Script ----------------->
+<script>
+
+export default {
+
+    //Data
+    data() {
+        return {
+            
+        }
+    },
+
+    //Props
+    props: {
+        pizzas: Array
+    }
+}
+
+
+</script>
+
+
+
+
+<!------------------ Template ----------------->
+<template>
+    <h1 class="text-center mb-5">Le pizze:</h1>
+
+
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div v-for="pizza in this.pizzas" class="col">
+            <div class="card text-center">
+                <img :src="pizza.url" class="card-img-top" :alt="pizza.nome">
+                <div class="card-body">
+                    <h5 class="card-title">{{ pizza.nome }}</h5>
+                    <p class="card-text">{{ pizza.descrizione }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+
+
+
+<!------------------ Style ----------------->
+<style lang="scss" scoped>
+
+.card {
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 15px;
+    height: 330px;
+
+    img {
+        width: 250px;
+    }
+}
+
+
+</style>
